fdae879849e0668c22e23e2875da0d6193493f79
Evan Butler & James Nielson <student@epicodus.com>
2015-04-08 11:08:58 -0700

    changeMachine function success

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,27 @@ var changeMachine = function(input) {
 
     var change = [];
 
-    var quarter_div = Math.floor(78/25)  + ' quarters';
+    var quarter_div = Math.floor(input/25)  + ' quarters';
+    var dime_div = '0 dimes';
+    var nickel_div = '0 nickels';
+    var pennies = '0 pennies';
     var quarter_mod = input % 25;
 
     if (quarter_mod !== 0) {
-        dime_div = quarter_mod / 10 + ' dimes';
-        dime_mod = quarter_mod % 10;
-    }
-    else (dime_mod !== 0) {
-        nickel_div = dime_mod / 5 + ' nickels';
-        nickel_mod = dime_mod % 5;
-    };
-    var pennies = nickel_mod + ' pennies';
+        var dime_div = Math.floor(quarter_mod / 10) + ' dimes';
+        var dime_mod = quarter_mod % 10;
 
-    change.push(quarter_div);
+        if (dime_mod !== 0) {
+            var nickel_div = Math.floor(dime_mod / 5) + ' nickels';
+            var nickel_mod = dime_mod % 5;
+
+            if (nickel_mod !== 0) {
+                var pennies = nickel_mod + ' pennies';
+            }
+        }
+
+    }
+    change.push(quarter_div, dime_div, nickel_div, pennies);
     console.log(change);
     return change;
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,26 @@
 describe("changeMachine", function() {
     it("returns 1 penny if the user enters 1", function() {
-        expect(changeMachine("1")).to.equal("1 penny");
+        expect(changeMachine("1")).to.eql(['0 quarters', '0 dimes', '0 nickels', '1 pennies']);
     });
 
     it("returns 1 nickel if the user enters 5", function() {
-        expect(changeMachine("5")).to.equal("1 nickel");
+        expect(changeMachine("5")).to.eql(['0 quarters', '0 dimes', '1 nickels', '0 pennies']);
     });
 
     it("returns 1 dime if the user enters 10", function() {
-        expect(changeMachine("10")).to.equal("1 dime");
+        expect(changeMachine("10")).to.eql(['0 quarters', '1 dimes', '0 nickels', '0 pennies']);
     });
 
     it("returns 1 quarter if the user enters 25", function() {
-        expect(changeMachine("25")).to.equal("1 quarter");
+        expect(changeMachine("25")).to.eql(['1 quarters', '0 dimes', '0 nickels', '0 pennies']);
     });
+
+    it("returns 1q, 1d, 1n, 1p if user enters 41", function() {
+        expect(changeMachine("41")).to.eql(['1 quarters', '1 dimes', '1 nickels', '1 pennies']);
+    });
+
+    it("returns 3q, 0d, 0n, 3p if user enters 78", function() {
+        expect(changeMachine("78")).to.eql(['3 quarters', '0 dimes', '0 nickels', '3 pennies']);
+    });
+
 });
